845f8984475fb72ce4c3d933e4e103ed6a824963
Halit AKAYDIN <63125040+halitAKAYDIN@users.noreply.github.com>
2022-09-15 09:45:26 +0300

    Update obfuscate.py

--- obfuscate.py
+++ obfuscate.py
@@ -20,15 +20,15 @@ file = open(path,'r')
 content = file.read()
 file.close()
 
-print ("[+]Generation of random encryption key")
+print ("[+] Generation of random encryption key")
 key = random.randint(1,196)
-print ("[+]Encode in UTF-16LE")
+print ("[+] Encode in UTF-16LE")
 encoded = content.encode("utf-16-le")
-print ("[+]Encrypt in XOR")
+print ("[+] Encrypt in XOR")
 encoded = xor(encoded, key)
-print ("[+]Encode in Base64")
+print ("[+] Encode in Base64")
 encoded = base64.b64encode(encoded)
-print ("[+]Your powershell is encoded : " + output)
+print ("[+] Your powershell is encoded : " + output)
 
 
 with open("stub.txt") as f:
